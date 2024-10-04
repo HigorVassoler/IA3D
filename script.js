52610e65e3e85a0c27b9c0758ddfe7d1346a183e
HigorVassoler <107952648+HigorVassoler@users.noreply.github.com>
2024-10-04 07:37:07 -0300

    .

--- script.js
+++ script.js
@@ -54,11 +54,10 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativa) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", ()=>respostaSelecionada(alternativa){
-            posicao++;
-            mostraPergunta();
-        });
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa);
         caixaAlternativa.appendChild(botaoAlternativas);
-    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    posicao++;
 }
 mostraPergunta();
