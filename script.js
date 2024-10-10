b29d3d839d1982510bb7811568e8a5564691852b
HigorVassoler <107952648+HigorVassoler@users.noreply.github.com>
2024-10-10 08:04:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,39 +5,42 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [
     {
-        enunciado: "Você acha que as IA's estão emburrecendo as pessoas?",
-        alternativas: [{
-            texto: "Sim, ",
-            afirmativa: "acredito que elas estão facilitando demais as coisas básicas."
-        },
-        {
-            texto: "Não, ",
-            afirmativa: "pois ela veio para auxiliar as pessoas."
-        }
+        enunciado: "Você gosta da ideia de Inteligência Artificial?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
         ]
     },
     {
-        enunciado: "Você acha que as IA's são algo que as pessoas devem temer futuramente?",
-        alternativas: [{
-            texto: "Sim, ",
-            afirmativa: "afirmativassss"
-        },
-        {
-            texto: "Não, ",
-            afirmativa: "Afirmativa da alternativa 2"
-        }
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
         ]
     },
     {
-        enunciado: "Você acredita que a tecnologia irá atrapalhar na juventude das pessoas?",
-        alternativas: [{
-            texto: "Sim, ",
-            afirmativa: "Afirmativa da alternativa 1"
-        },
-        {
-            texto: "Não, ",
-            afirmativa: "Afirmativa da alternativa 2"
-        }
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
         ]
     }
 ]
@@ -52,15 +55,16 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativa) {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa);
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmativa;
+    respostas = afirmacoes;
     posicao++;
     mostraPergunta();
 }
